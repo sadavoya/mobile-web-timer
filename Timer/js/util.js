0266e0b765d5e1d9a7166f5270aac40d64062e73
Sadavoya <sadavoya@gmail.com>
2014-05-25 11:07:38 -0600

    Cleaned up the Util namespace.

--- Timer/js/util.js
+++ Timer/js/util.js
@@ -27,78 +27,89 @@
         return;
     }
     var my_ns = 'util',
-        ns = window.myApp;
+        ns = window.myApp,
 
+        // Add 'method' method, for adding new methods to object prototypes
+        method = (function () {
+            function method(o, name, func) {
+                o.prototype[name] = func;
+                return this;
+            }
+            return method;
+        })(),
 
-    // Add 'method' method to Function prototype, for adding new methods to Function
-    var method = (function () {
-        function method(o, name, func) {
-            o.prototype[name] = func;
-            return this;
-        }
-        return method;
-    })();
+        // Returns a curried version of the function - one where some but not necessarily all parameters have been provided
+        curry = (function () {
+            function curry() {
+                var slice = Array.prototype.slice,
+                    args = slice.apply(arguments),
+                    that = this;
+                return function () {
+                    return that.apply(null, args.concat(slice.apply(arguments)));
+                };
+            }
+            return curry;
+        })(),
 
-    // Add curry method to all functions
-    method(Function, 'curry',
-        // Returns a curried version of the function - one where some but not all parameters have been provided
-        function () {
-            var slice = Array.prototype.slice,
-                args = slice.apply(arguments),
-                that = this;
-            return function () {
-                return that.apply(null, args.concat(slice.apply(arguments)));
-            };
-        });
-    // Add wrap method to all strings
-    method(String, 'wrap',
         // Returns the string with wrapper as both prefix and postfix 
-        function (wrapper) {
-            return (wrapper + this + wrapper);
-        });
+        wrap = (function () {
+            function wrap(wrapper) {
+                return (wrapper + this + wrapper);
+            }
+            return wrap;
+        })(),
 
-    // Add a pad method to all strings
-    method(String, 'pad',
         // Pad the string, on the left or the right side, 
         // with a filler string until the string length equals or exceeds len. Return the new string.
-        function (fill, len, rightside) {
-            var result = this;
-            if (fill && len) {
-                while (result.length < len) {
-                    if (rightside) {
-                        result = result + fill;
-                    } else {
-                        result = fill + result;
+        pad = (function () {
+            function pad(fill, len, rightside) {
+                var result = this;
+                if (fill && len) {
+                    while (result.length < len) {
+                        if (rightside) {
+                            result = result + fill;
+                        } else {
+                            result = fill + result;
+                        }
                     }
                 }
+                return (result);
             }
-            return (result);
-        });
+            return pad;
+        })(),
 
-    // Returns a function that checks if predicate returns true, and if so calls action
-    var doIf = (function () {
-        function doIf(predicate, action) {
-            return function (e, o) {
-                if (!predicate || predicate(e, o)) {
-                    action();
+        // Returns a function that checks if predicate returns true, and if so calls action
+        doIf = (function () {
+            function doIf(predicate, action) {
+                return function (e, o) {
+                    if (!predicate || predicate(e, o)) {
+                        action();
+                    }
+                    e.preventDefault();
+                };
+            }
+            return doIf;
+        })(),
+
+        // Set the specified checkbox's "checked" state
+        setCheckbox = (function () {
+            function setCheckbox(checkbox, checked) {
+                if (checked) {
+                    checkbox.attr('checked', 'checked');
+                } else {
+                    checkbox.removeAttr('checked');
                 }
-                e.preventDefault();
-            };
-        }
-        return doIf;
-    })();
-
-    // Set the specified checkbox's "checked" state
-    var setCheckbox = (function () {
-        function setCheckbox(checkbox, checked) {
-            if (checked) {
-                checkbox.attr('checked', 'checked');
-            } else {
-                checkbox.removeAttr('checked');
             }
-        }
-        return setCheckbox;
-    })();
+            return setCheckbox;
+        })();
+
+    // Add curry method to all functions
+    method(Function, 'curry', curry);
+    // Add wrap method to all strings
+    method(String, 'wrap', wrap);
+    // Add a pad method to all strings
+    method(String, 'pad', pad);
+
     // Update the namespace with public methods
     (function () {
         ns.add('doIf', doIf, my_ns);
